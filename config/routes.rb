46bb711e965cbbe84b7215b98306566ac4fe7c41
Alexander Kratzsch <alex@devrandom.se>
2015-01-29 18:45:01 +0100

    added and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	resources :entries, only: [:index, :destroy] do
+		get 'filter', on: :collection
+	end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
