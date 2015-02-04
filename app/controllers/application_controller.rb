16415dcf29ecc3f8f531b6f0f95736f846faa5bc
Alexander Kratzsch <alex@devrandom.se>
2015-02-04 18:01:51 +0100

    ready for public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with realm: "Helplist administration", name: "nsastaff", password: "Syp9595", if: :need_authentication?
+  http_basic_authenticate_with realm: "Helplist administration", name: "username", password: "password", if: :need_authentication?
 
   before_action :check_break
 
